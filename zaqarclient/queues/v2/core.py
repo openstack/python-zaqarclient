4010451e18103dc181600a0f7385c1dbe5d7a8b5
Ryan S. Brown <sb@ryansb.com>
2015-11-21 07:36:46 -0500

    Support creating signed url's in client
    This change adds a function to create signed URLs, but does not create a
    domain object or add them to the CLI.
    
    Change-Id: I14d3c6aa6d7a5c8c5bf732e86cf9a24ca1342fad

--- zaqarclient/queues/v2/core.py
+++ zaqarclient/queues/v2/core.py
@@ -27,6 +27,11 @@ Functions present in this module assume that:
     request.
 """
 
+import datetime
+import json
+
+from oslo_utils import timeutils
+
 from zaqarclient.queues.v1 import core
 
 queue_create = core.queue_create
@@ -41,3 +46,46 @@ message_list = core.message_list
 message_post = core.message_post
 message_delete = core.message_delete
 message_delete_many = core.message_delete_many
+
+
+def signed_url_create(transport, request, queue_name, paths=None,
+                      ttl_seconds=None, project_id=None, methods=None):
+    """Creates a signed URL given a queue name
+
+    :param transport: Transport instance to use
+    :type transport: `transport.base.Transport`
+    :param request: Request instance ready to be sent.
+    :type request: `transport.request.Request`
+    :param queue_name: name of Queue for the URL to access
+    :type name: `six.text_type`
+    :param paths: Allowed actions. Options: messages, subscriptions, claims
+    :type name: list
+    :param ttl_seconds: Seconds the URL will be valid for, default 86400
+    :type name: int
+    :param project_id: defaults to None
+    :type name: `six.text_type`
+    :param methods: HTTP methods to allow, defaults to ["GET"]
+    :type name: `list`
+    """
+
+    request.operation = 'signed_url_create'
+    request.params['queue_name'] = queue_name
+
+    body = {}
+    if ttl_seconds is not None:
+        expiry = (timeutils.utcnow() + datetime.timedelta(seconds=ttl_seconds))
+        body['expires'] = expiry.isoformat()
+
+    if project_id is not None:
+        body['project_id'] = project_id
+
+    if paths is not None:
+        body['paths'] = paths
+
+    if methods is not None:
+        body['methods'] = methods
+
+    request.content = json.dumps(body)
+
+    resp = transport.send(request)
+    return resp.deserialized_content
