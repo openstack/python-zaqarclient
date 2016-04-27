bb08d6ad37a1f1f9f2df16cc90d41b796293aebb
Thomas Bechtold <tbechtold@suse.com>
2016-04-27 09:12:20 -0500

    Fix doc build if git is absent
    When building packages if git is absent, then we should not set
    html_last_updated_fmt. It can still be set via the -D switch
    when building with sphinx-build.
    
    Change-Id: I1e1865dfcd76eb7f06dd28a5b3b401c5625fcdde
    Closes-Bug: #1552251

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -15,6 +15,7 @@
 import sys
 import os
 import subprocess
+import warnings
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
@@ -161,8 +162,13 @@ html_static_path = []
 #html_last_updated_fmt = '%b %d, %Y'
 git_cmd = ["git", "log", "--pretty=format:'%ad, commit %h'",
            "--date=local", "-n1"]
-html_last_updated_fmt = subprocess.Popen(
-    git_cmd, stdout=subprocess.PIPE).communicate()[0]
+try:
+    html_last_updated_fmt = subprocess.Popen(
+        git_cmd, stdout=subprocess.PIPE).communicate()[0]
+except Exception:
+    warnings.warn('Cannot get last updated time from git repository. '
+                  'Not setting "html_last_updated_fmt".')
+
 # If true, SmartyPants will be used to convert quotes and dashes to
 # typographically correct entities.
 #html_use_smartypants = True
