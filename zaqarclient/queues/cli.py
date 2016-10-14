7609046583fd38aae954295ebb41dfc28836c4f1
KATO Tomoyuki <kato.tomoyuki@jp.fujitsu.com>
2016-10-14 13:54:35 +0900

    Add plug-in summary for osc doc
    Stevedore Sphinx extension handles this comment.
    http://docs.openstack.org/developer/python-openstackclient/plugin-commands.html
    
    Change-Id: I57cf816c30fadc021214725c5018363e66d5c741

--- zaqarclient/queues/cli.py
+++ zaqarclient/queues/cli.py
@@ -12,6 +12,8 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
+"""OpenStackClient plugin for Messaging service."""
+
 import logging
 
 from osc_lib import utils
